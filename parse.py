2197d37466233ab16d899b57dc91d398529e1085
gregrr@gmail.com <gregrr@gmail.com>
2022-10-22 19:31:47 -0400

    basic functionality in one file, needs comments and cleanup and proper output formats

--- parse.py
+++ parse.py
@@ -1,10 +1,13 @@
-import colorgram
 import os
+from bs4 import BeautifulSoup
+
+import colorgram
+
 from pathlib import Path
 import torch
 from carvekit.api.high import HiInterface
 
-debug = True
+debug = False
 
 def rgb_to_hex(rgb):
     return '%02x%02x%02x' % rgb
@@ -33,23 +36,49 @@ if debug:
     os.system("mkdir jpg")
     os.system("mkdir png")
 
+
+
+
+os.system(f"cat *.html > combined_html; mkdir htmls; mv *.html htmls/")
+
+f = open("combined_html", "r")
+
+soup = BeautifulSoup(f,'html.parser')
+
+company = soup.find('span', attrs={"class":"breadcrumbs__crumb--active"}).get_text().strip()
+
+colorways = soup.findAll('div', attrs={"class":"yarn__colorway__preview"})
+
 #set up the image segmenter (removes the subject from the background)
 interface = HiInterface(batch_size_seg=1, batch_size_matting=1, device='cuda' if torch.cuda.is_available() else 'cpu', seg_mask_size=320, matting_mask_size=2048)
 
-#get all the files in the current directory
-for filename in os.listdir("."):
-    if filename.endswith('jpg') and os.path.isfile(filename) and os.path.getsize(filename) > 100: #filter for files that have jpg extensions, are files not folders, and have a size that makes sense
-        filename_without_extension = Path(filename).stem #get the base filename without extensions -- ie asdf.jpg to asdf
+for color in colorways:
+    name = color.find('div', attrs={"class":"yarn__colorway__preview__title"}).get_text().strip()
+    link = color.find('a', href=True)
+    link = link['href']
+    image = color.find('img', src=True)
+    image_og = image['src']
+    image = image['src'].replace("_small.j", ".j")
+    image_save_name = image.lower().replace("/","_").replace(":","").replace("jpeg","jpg")
+    if not image_save_name.endswith('.jpg'):
+        image_save_name = f"{image_save_name}.jpg"
+    
+    os.system(f"wget -q {image} -O {image_save_name}")
+    if os.path.getsize(image_save_name) > 100:
+        # start color extraction
+
+
+        filename_without_extension = Path(image_save_name).stem #get the base filename without extensions -- ie asdf.jpg to asdf
 
         #run the image segmentation
-        images_without_background = interface([filename])                   
+        images_without_background = interface([image_save_name])                   
         image_without_background = images_without_background[0]
 
         color = get_primary_color( image_without_background ) #get the primary color from the segmented image
 
 
         if color is None: #if we didn't get a color, try re-running but on the original image, not the segmented one
-            color = get_primary_color(f"{filename}")
+            color = get_primary_color(f"{image_save_name}")
 
         
         #sometimes if there isn't really a subject in the image (ie fully zoomed in picture of yarn), segmentation will return nonsese.
@@ -63,18 +92,26 @@ for filename in os.listdir("."):
         totalUniqueColors = len(uniqueColors)
 
         if totalUniqueColors <10: #if we didn't many unique colors in the segmented file we'll re run on just the raw file
-            color = get_primary_color(f"{filename}")
+            color = get_primary_color(f"{image_save_name}")
      
         if debug:
             image_without_background.save(f"{filename_without_extension}.png")
 
             print(f"pixels {w} {h} ; unique colors: {totalUniqueColors}")
 
-            os.system(f"convert {filename} -resize 500x500 -background '#{color}' label:'#{color}' -gravity Center -append {filename_without_extension}-color.jpg")
+            os.system(f"convert {image_save_name} -resize 500x500 -background '#{color}' label:'#{color}' -gravity Center -append {filename_without_extension}-color.jpg")
             os.system(f"convert {filename_without_extension}.png -resize 500x500 -background '#{color}' label:'#{color}' -gravity Center -append {filename_without_extension}-color.png")
 
         if debug:
-            os.system(f"mv {filename} jpg/")
+            os.system(f"mv {image_save_name} jpg/")
             os.system(f"mv {filename_without_extension}.png png/")
 
-        print(f"Segmented & Color Found: #{color} - {filename}")
+        #print(f"Segmented & Color Found: #{color} - {image_save_name}")
+
+
+
+
+
+        #end color extraction
+        print(f"{company} - {name} - {link} - #{color}")
+        print("\n===================================================\n")
